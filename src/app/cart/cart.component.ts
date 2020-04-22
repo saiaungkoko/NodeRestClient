df0eb3b70c0a4321dc93d26fdad79e52e290e364
saiaungkoko <saiaungkoko@mail.fresnostate.edu>
2020-04-22 15:36:26 -0700

    finish client

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
 import { ToastService } from '../toast/toast.service';
+import { HttpService } from '../../shared-service/http.service';
 
 export interface IBike {
   id?: number;
@@ -20,15 +21,60 @@ export class CartComponent implements OnInit {
 
   bikes: Array<IBike> = [];
   myName = '';
+  cars = [];
   constructor(
     private activatedRoute: ActivatedRoute,
     private router: Router,
-    private toastService: ToastService
+    private toastService: ToastService,
+    private http: HttpService
   ) { }
 
   async ngOnInit() {
-
+    await this.refresh();
+    // this.createCar('car', {make: 'Tesla', model: 'X'});
+    // this.updateCar('car/id/1', { make: 'Ford', model: 'Fiasta'});
   }
 
+  async refresh() {
+    this.cars = await this.getCars('car');
+  }
+
+  async getCars(path: string) {
+    const resp = await this.http.get(path);
+    return resp;
+  }
+
+  async createCar() {
+    const car = {
+      make: null,
+      model: null,
+      year: null
+    };
+    const resp = await this.http.post('car', car);
+    if (resp) {
+      // this.refresh();
+      this.cars.unshift(resp);
+    } else {
+      this.toastService.showToast('danger', 3000, 'Car create failed!');
+    }
+    return resp;
+  }
+
+  async updateCar(car: any) {
+    const resp = await this.http.put(`car/id/${car.id}`, car);
+    if (resp) {
+      this.toastService.showToast('success', 3000, 'Car updated successfully!');
+    }
+    return resp;
+  }
+
+  async removeCar(car: any, index: number) {
+    const resp = await this.http.delete(`car/id/${car.id}`);
+    if (resp) {
+      this.refresh();
+    } else {
+      this.toastService.showToast('danger', 3000, 'Delete car failed!');
+    }
+  }
 
 }
--- src/shared-service/http.service.ts
+++ src/shared-service/http.service.ts
@@ -1,14 +1,48 @@
 import { Injectable } from '@angular/core';
-import { Headers } from '@angular/http';
+import { Http, Headers } from '@angular/http';
+import { environment } from '../environments/environment';
+import { CombineLatestOperator } from 'rxjs/internal/observable/combineLatest';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
-
+  apiURL = '';
   constructor(
-
+    private http: Http
   ) {
+    this.apiURL = environment.apiURL;
+  }
+
+  testing() {
+  }
+
+  async get(path: string) {
+    const resp = await this.http.get(this.apiURL + path, this.headers).toPromise();
+    return resp.json();
+  }
+
+  async post(path: string, payload: any) {
+    const resp = await this.http.post(this.apiURL + path, payload, this.headers).toPromise();
+    return resp.json();
+  }
+
+  async put(path: string, payload: any) {
+    const resp = await this.http.put(this.apiURL + path, payload, this.headers).toPromise();
+    return resp.json();
+  }
+
+  async delete(path: string) {
+    const resp = await this.http.delete(this.apiURL + path, this.headers).toPromise();
+    return resp.json();
+  }
+
+  async logout() {
+    const resp: any = await this.http.get(this.apiURL + 'user/logout', this.headers).toPromise();
+    if (resp.statusCode === 200) {
+      localStorage.removeItem('id-token');
+    }
+    return resp.json();
   }
 
   get headers() {
